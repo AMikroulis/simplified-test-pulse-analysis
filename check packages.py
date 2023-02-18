c13b1f89124c37029d6fcf2e5f7e1ff6c03427b6
Apostolis <49730148+AMikroulis@users.noreply.github.com>
2023-02-18 13:48:51 +0100

    Update check packages.py

--- check packages.py
+++ check packages.py
@@ -2,7 +2,7 @@ from pkgutil import iter_modules
 
 installed = list(x[1] for x in iter_modules())
 
-required_packages = ['ast', 'datetime','numpy','scipy','matplotlib','sklearn','os','pyabf','threading','PyQt5','subprocess']
+required_packages = ['ast', 'datetime','numpy','scipy','matplotlib','os','pyabf','threading','PyQt5','subprocess']
 
 for rqpkg in required_packages:
     if rqpkg in installed:
